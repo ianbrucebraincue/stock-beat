ede2983394758f2de0eb76e11db487bc71fae0b1
Ian Bruce <ianbruce.braincue@gmail.com>
2023-09-28 13:39:01 -0400

    add: axios, stock-api hook, test fetch to json placeholder posts and printed to page.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
+import StockAPI from './hooks/use-stock-api';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <StockAPI />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/hooks/use-stock-api.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
+export default function StockAPI () {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+          setPosts(response.data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }, []);
+
+    return (
+        <ul>
+            {posts.map(post => (
+                <li key={post.id}>{post.title}</li>
+            ))}
+        </ul>
+    );
+}
