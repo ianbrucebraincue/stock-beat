4373c6fc95003559a1b8968c96c2dbec0bcf2523
Ian Bruce <ianbruce.braincue@gmail.com>
2023-10-20 19:02:05 -0400

    wip: scale that can move in reverse. as the patterName up and down don't reverse from current index along a set scale

--- src/components/synthesizer.js
+++ src/components/synthesizer.js
@@ -3,17 +3,7 @@ import * as Tone from 'tone';
 
 export default function Synthesizer( { filteredApiData, data } ) {
     const [synth, setSynth] = useState({});
-    const MIDI_NUM_NAMES = ["C_1", "C#_1", "D_1", "D#_1", "E_1", "F_1", "F#_1", "G_1", "G#_1", "A_1", "A#_1", "B_1",
-                "C0", "C#0", "D0", "D#0", "E0", "F0", "F#0", "G0", "G#0", "A0", "A#0", "B0",
-                "C1", "C#1", "D1", "D#1", "E1", "F1", "F#1", "G1", "G#1", "A1", "A#1", "B1",
-                "C2", "C#2", "D2", "D#2", "E2", "F2", "F#2", "G2", "G#2", "A2", "A#2", "B2",
-                "C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3",
-                "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4",
-                "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5",
-                "C6", "C#6", "D6", "D#6", "E6", "F6", "F#6", "G6", "G#6", "A6", "A#6", "B6",
-                "C7", "C#7", "D7", "D#7", "E7", "F7", "F#7", "G7", "G#7", "A7", "A#7", "B7",
-                "C8", "C#8", "D8", "D#8", "E8", "F8", "F#8", "G8", "G#8", "A8", "A#8", "B8",
-                "C9", "C#9", "D9", "D#9", "E9", "F9", "F#9", "G9"];
+    // thanks to GuitarLand https://www.guitarland.com/MusicTheoryWithToneJS/PlayMajorScale.html
     const MIDI_SHARP_NAMES = ['B#_0',  'C#_1', 'Cx_1', 'D#_1',   'E_1',  'E#_1',  'F#_1', 'Fx_1',  'G#_1', 'Gx_1', 'A#_1', 'B_1',
                 'B#_1', 'C#0', 'Cx0', 'D#0', 'E0', 'E#0', 'F#0', 'Fx0', 'G#0', 'Gx0', 'A#0', 'B0',
                 'B#0', 'C#1', 'Cx1', 'D#1', 'E1', 'E#1', 'F#1', 'Fx1', 'G#1', 'Gx1', 'A#1', 'B1',
@@ -50,7 +40,7 @@ export default function Synthesizer( { filteredApiData, data } ) {
         return Number(MIDInumber); // it should be a number already, but...
     }
     
-    const MAJOR_SCALE = [0, 2, 4, 5, 7, 9, 11, 12];
+    const MAJOR_SCALE = [0, 2, 4, 5, 7, 9, 11, 12, 14, 16, 17, 19, 21, 23, 24];
 
     function makeScale(scaleFormula, keyNameAndOctave) {
         var ALPHA_NAMES = ['A','B','C','D','E','F','G'];
@@ -71,10 +61,8 @@ export default function Synthesizer( { filteredApiData, data } ) {
         for(var j = 0; j < myScaleFormula.length; j++) {
             if(MIDI_SHARP_NAMES[myScaleFormula[j] + startingNote].includes(ALPHA_NAMES[(offset + j) % ALPHA_NAMES.length])) {
                 madeScale.push( MIDI_SHARP_NAMES[myScaleFormula[j] + startingNote] );
-                // console.log("one");
             } else if(MIDI_FLAT_NAMES[myScaleFormula[j] + startingNote].includes(ALPHA_NAMES[(offset + j) % ALPHA_NAMES.length])) {
                 madeScale.push( MIDI_FLAT_NAMES[myScaleFormula[j] + startingNote] );
-                // console.log("two");
             } else {
                 madeScale.push("C7"); // high note used to indicate error
             }
@@ -92,17 +80,17 @@ export default function Synthesizer( { filteredApiData, data } ) {
             // const now = Tone.now(); // time keeper
             var patternName = "up";
             var tempo = '60';
-            var volume = '-15';
+            var volume = '-10';
             
             Tone.context.resume().then(() => {
                 // if (filteredApiData.length) {
                     // Ab, A, Bb, B, Cb, C, C#, Db, D, Eb, E, F, F#, Gb, G
-                    var scale = makeScale(MAJOR_SCALE, 'C#4'); 
+                    var scale = makeScale(MAJOR_SCALE, 'C4'); 
                     // console.log(scale);
-
                     var pattern = new Tone.Pattern(function(time, note) {
                         newSynth.triggerAttackRelease(note, "4n", time);
                     }, scale, patternName).start(0);
+                    console.log(pattern);
 
                     Tone.Transport.bpm.value = tempo;   
                     newSynth.volume.value = volume;
@@ -124,7 +112,7 @@ export default function Synthesizer( { filteredApiData, data } ) {
         <>
         <div>   
             <h1>Synthesizer</h1>
-            <button onClick={playSynth}>Play C4</button>
+            <button onClick={playSynth}>Play Scale</button>
         </div>
         </>
     );
